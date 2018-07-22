22e27e535d44b71cb8a6dd78e0ea3923d42db579
kwonhong <mkh9012@kookmin.ac.kr>
2018-07-22 19:15:49 +0900

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/mobile/a21line/Login/Join_Lost_Activity.java
+++ app/src/main/java/com/mobile/a21line/Login/Join_Lost_Activity.java
@@ -17,10 +17,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
+import com.mobile.a21line.MyBid.MyBid_Listitem;
 import com.mobile.a21line.R;
 import com.mobile.a21line.SaveSharedPreference;
 import com.mobile.a21line.VolleySingleton;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -105,6 +107,47 @@ public class Join_Lost_Activity extends AppCompatActivity {
                 }
             }
         });
+
+        et_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findMemID();
+            }
+        });
+    }
+
+    private void findMemID(){
+        if(et_email.getText().toString().isEmpty()){
+            Toast.makeText(mContext, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        RequestQueue postRequestQueue = VolleySingleton.getInstance(mContext).getRequestQueue();
+        StringRequest postJsonRequest = new StringRequest(Request.Method.POST, SaveSharedPreference.getServerIp() + "/Member/findMemID.do", new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response){
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    String MemID = obj.optString("MemID", "");
+                    if(MemID.isEmpty()){
+                        Toast.makeText(mContext, "해당하는 이메일로 가입된 회원이 없습니다.", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(mContext, "회원님의 아이디는 '" + MemID + "' 입니다.", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+                catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, SaveSharedPreference.getErrorListener(mContext)) {
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap();
+                params.put("sEmail", et_email.getText().toString());
+                return params;
+            }
+        };
+        postRequestQueue.add(postJsonRequest);
     }
 
 
